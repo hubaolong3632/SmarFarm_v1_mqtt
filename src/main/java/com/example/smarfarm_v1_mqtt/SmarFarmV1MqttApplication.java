423487d80e68acbb4ff91d012ada384702146163
hubaolong@lv-ju.com <hubaolong@lv-ju.com>
2025-11-05 16:25:36 +0800

    qe

---
+++ src/main/java/com/example/smarfarm_v1_mqtt/SmarFarmV1MqttApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarfarm_v1_mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarFarmV1MqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarFarmV1MqttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smarfarm_v1_mqtt/SmarFarmV1MqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarfarm_v1_mqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarFarmV1MqttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
